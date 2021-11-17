32555cf0aee4d30da81ba509885e515bc952ff60
gabriben9 <gabriel10043@live.com>
2021-11-17 08:15:39 -0300

    Create lista.h

---
+++ lista.h
@@ -0,0 +1,27 @@
+
+
+typedef struct item{
+
+	int chave;
+	char caractere;
+	struct item *proximo;
+
+} Item;
+
+typedef struct lista{
+
+	Item *primeiro;
+	Item *ultimo;
+	int tamanho;
+
+}Lista;
+
+Lista* criaListaVazia();
+Item* criaItem(int, char);
+void inserir(Lista*, Item*);
+int listaVazia(Lista*);
+int removeElemento(Lista*, int);
+void imprimeLista(Lista*);
+Item* buscarElemento(Lista*, char);
+void moverInicio(Lista*, int);
+void deletaLista(Lista*);
