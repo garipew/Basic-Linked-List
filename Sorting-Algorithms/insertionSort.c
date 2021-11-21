3ddc1b7cc8e18ef5d74bac0c9fd4c067120ac179
Gabriel Sá <gabriel10043@live.com>
2021-11-21 08:50:51 -0300

    Create insertionSort.c

---
+++ Sorting-Algorithms/insertionSort.c
@@ -0,0 +1,19 @@
+void insertionSort(Lista* l){
+	
+	Item* i = l->primeiro->proximo;
+	Item* j;
+
+	while(i != l->ultimo->proximo){
+
+		j = i;
+		while(j != l->primeiro->proximo){
+					if(j->chave > j->anterior->chave){
+						troca(l, j, j->anterior);
+          } else{
+            j = j->anterior;
+          }
+    }
+    
+    i = i->proximo;
+  }
+  
